d58328dcf2980831c59cff88a04f8928f38aa2b4
xDarkN <nadavedri1372@gmail.com>
2023-08-08 14:17:27 +0300

    f

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-    if HelloWorld() != "Hello World!!" {
-        t.Fatal("Test fail")
-    }
+	if helloworld() != "Hello World!!" {
+		t.Fatal("Test fail")
+	}
 }
